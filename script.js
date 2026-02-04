762237089d9ec7e3fdde983887a6b47fed6c4c72
AkashAdak-coder <akashadak2608@gmail.com>
2026-02-05 01:44:26 +0530

    add some function

---
+++ script.js
@@ -0,0 +1,8 @@
+// Grab elements
+const newNoteBtn = document.getElementById('newNoteBtn');
+const notesList = document.querySelector('.notes-list');
+const noteTitleInput = document.getElementById('note-title');
+const noteContentDiv = document.getElementById('note-content');
+const contentBox = document.querySelector('.content .input-box');
+
+
